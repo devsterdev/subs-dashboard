76216e055cf1923dbc8a0760c5281fe53a6bc823
devdeepsharma <sharmadevdeep9@gmail.com>
2025-11-24 22:24:30 +0530

    Search feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-
-import './App.css'
-import Navbar from './components/Navbar'
-import List from './components/List'
-import SearchElm from './components/Search'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import List from "./components/List";
+import SearchElm from "./components/Search";
+import data from "./data/data.json";
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  const filteredData = data.filter(
+    (item) =>
+      item.name.toLowerCase().includes(query.toLowerCase()) ||
+      item.uname.toLowerCase().includes(query.toLowerCase())
+  );
 
   return (
     <>
-    <div className='container mx-auto px-50 py-8'>
-      <Navbar />
-      <SearchElm />
-      <div className='grid grid-cols-3 gap-4'>
-        <List name="Vsause" uname="vsause" subs="24M" description="This is my fav channel" url="*" />  
-        <List name="Vsause" uname="vsause" subs="24M" description="This is my fav channel" url="*" />  
-        <List name="Vsause" uname="vsause" subs="24M" description="This is my fav channel" url="*" />  
-        <List name="Vsause" uname="vsause" subs="24M" description="This is my fav channel" url="*" />  
-        <List name="Vsause" uname="vsause" subs="24M" description="This is my fav channel" url="*" />  
-        <List name="Vsause" uname="vsause" subs="24M" description="This is my fav channel" url="*" />  
-        <List name="Vsause" uname="vsause" subs="24M" description="This is my fav channel" url="*" />  
-        <List name="Vsause" uname="vsause" subs="24M" description="This is my fav channel" url="*" />  
-        <List name="Vsause" uname="vsause" subs="24M" description="This is my fav channel" url="*" />  
-        <List name="Vsause" uname="vsause" subs="24M" description="This is my fav channel" url="*" />  
-        <List name="Vsause" uname="vsause" subs="24M" description="This is my fav channel" url="*" />  
+      <div className="container mx-auto px-50 py-8">
+        <Navbar />
+        <SearchElm setQuery={setQuery} />
+        <div className="grid grid-cols-3 gap-4">
+          {filteredData.map((e) => (
+            <List
+              name={e.name}
+              uname={e.uname}
+              subs={e.subs}
+              description={e.description}
+              url={e.url}
+              key={e.uname}
+            />
+          ))}
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import { Search } from 'lucide-react';
+import React, { useEffect, useState } from "react";
+import { Search } from "lucide-react";
 
-const SearchElm = () => {
+const SearchElm = ({ setQuery }) => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    const text = e.target.value;
+    setValue(text);
+    setQuery(text);
+  };
 
   return (
     <div className="max-w-md relative group w-full m-8">
       <input
         type="text"
-        placeholder="SEARCH STACK..." 
-        // value={value}
-        // onChange={onChange}
+        placeholder="SEARCH STACK..."
+        value={value}
+        onChange={handleChange}
         className="w-full bg-white border-4 border-black px-4 py-3 text-lg font-bold uppercase placeholder-gray-400 outline-none shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] focus:translate-x-[2px] focus:translate-y-[2px] focus:shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] transition-all"
       />
       <Search className="absolute right-4 top-1/2 -translate-y-1/2 w-6 h-6 text-black pointer-events-none" />
     </div>
-  )
-}
+  );
+};
 
-export default SearchElm
+export default SearchElm;
